932c696a9fc59c9a8368b6b5aa57a3009bbfe4df
Mohamed <muhmis13@gmail.com>
2025-03-04 22:46:21 -0800

    Add submitions mentions bar chart

--- server.R
+++ server.R
@@ -3,15 +3,15 @@ library(DBI)
 library(duckdb)
 library(dplyr)
 library(plotly)
-library(lubridate)  # For hour() function
+library(lubridate)
 
 con <- dbConnect(duckdb(), dbdir = ":memory:")
-file_path <- "/Users/sakaria/Desktop/BIS 412/DataVis1/NYC 311/311-dataset-POSIXct.parquet"
+file_path <- "C:/Users/muhmi/Github re/311 Data/311-dataset-POSIXct.parquet"
 nyc311_dataset <- tbl(con, paste0("read_parquet('", file_path, "')"))
 
 shinyServer(function(input, output, session) {
   
-  # 1) Top 5 agencies
+  # Top 5 agencies
   top_agencies <- reactive({
     nyc311_dataset %>%
       group_by(agency) %>%
@@ -20,14 +20,12 @@ shinyServer(function(input, output, session) {
       head(5) %>%
       collect()
   })
-  
   output$agencyPlot <- renderPlotly({
     data <- top_agencies()
-    plot_ly(data, x = ~agency, y = ~requests, type = "bar") %>%
-      layout(title = "Top 5 Agencies")
+    plot_ly(data, x = ~agency, y = ~requests, type = "bar")
   })
   
-  # 2) Hourly complaints
+  # Hourly complaints
   hour_counts <- reactive({
     nyc311_dataset %>%
       select(created_date) %>%
@@ -36,12 +34,26 @@ shinyServer(function(input, output, session) {
       group_by(hour_of_day) %>%
       summarise(request_count = n())
   })
-  
   output$hourPlot <- renderPlotly({
     data <- hour_counts()
-    plot_ly(data, x = ~hour_of_day, y = ~request_count,
-            type = "scatter", mode = "lines+markers") %>%
-      layout(title = "Complaints by Hour of Day")
+    plot_ly(data, x = ~hour_of_day, y = ~request_count, 
+            type = "scatter", mode = "lines+markers")
+  })
+  
+  # Submission methods
+  submission_methods <- reactive({
+    nyc311_dataset %>%
+      filter(open_data_channel_type != "OTHER") %>%
+      group_by(open_data_channel_type) %>%
+      summarise(request_count = n()) %>%
+      arrange(desc(request_count)) %>%
+      collect()
+  })
+  output$submissionPlot <- renderPlotly({
+    data <- submission_methods()
+    plot_ly(data, x = ~open_data_channel_type, y = ~request_count, 
+            type = "bar") %>%
+      layout(title = "Submission Methods")
   })
   
 })
--- ui.R
+++ ui.R
@@ -3,21 +3,24 @@ library(plotly)
 
 shinyUI(fluidPage(
   
-  titlePanel("NYC 311 Requests - Version 2"),
+  titlePanel("NYC 311 Requests - Version 3"),
   
   sidebarLayout(
     sidebarPanel(
       radioButtons("vizChoice", "Choose Visualization:",
-                   choices = list("Top Agencies" = "agency", "By Hour" = "hour"))
+                   choices = list("Top Agencies" = "agency", 
+                                  "By Hour" = "hour",
+                                  "Submission Methods" = "submission"))
     ),
     mainPanel(
-      conditionalPanel(
-        condition = "input.vizChoice == 'agency'",
-        plotlyOutput("agencyPlot")
+      conditionalPanel(condition = "input.vizChoice == 'agency'",
+                       plotlyOutput("agencyPlot")
       ),
-      conditionalPanel(
-        condition = "input.vizChoice == 'hour'",
-        plotlyOutput("hourPlot")
+      conditionalPanel(condition = "input.vizChoice == 'hour'",
+                       plotlyOutput("hourPlot")
+      ),
+      conditionalPanel(condition = "input.vizChoice == 'submission'",
+                       plotlyOutput("submissionPlot")
       )
     )
   )
